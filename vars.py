d5f3edc066842c7e723022661438824fe47a76bc
boollywoodbot <geetmp3s@gmail.com>
2025-06-27 23:51:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "21905616"))
 API_HASH = environ.get("API_HASH", "0506d1a8b04f4c580c369b47c885bbd4")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7848383006:AAHt8R3pe4AteA06OF-JMua0ZYYtZF2GsGc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8126788658:AAFd57pq2CiSn0-T1c51ig79ji4k230nrH4")
 OWNER = int(environ.get("OWNER", "7912270773"))
 CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
 LOG_CHANNEL = "-100" # log group me bot ko admin narur banaye full rights ke sath
